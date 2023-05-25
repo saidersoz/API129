8f5827e87b1cdc61311875e94940114d6ee0f397
saidErsoz <m.said.ersoz3443@gmail.com>
2023-05-25 18:43:32 +0300

    25/05

---
+++ src/test/java/Get_Requests/Get05.java
@@ -0,0 +1,44 @@
+package Get_Requests;
+
+import base_urls.HerokuAppBaseUrl;
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertTrue;
+
+public class Get05 extends HerokuAppBaseUrl {
+
+    /*
+    Given
+        https://restful-booker.herokuapp.com/booking
+    When
+        User sends a GET request to the URL
+    Then
+        Status code is 200
+    And
+        Among the data there should be someone whose firstname is "Sally" and last name is "Brown"
+              (Data içerisinde firstname değeri "Sally", lastname değeri "Brown" olan biri olmalı)
+ */
+
+    @Test
+    public void Get05() {
+
+        spec.pathParam("first","booking").
+                queryParams("firstname","Sally","lastname","Brown");
+
+        Response response = given(spec).accept(ContentType.JSON).get("{first}");
+        response.prettyPrint();
+
+        response.then().
+                statusCode(200).body("bookingid",hasSize(greaterThan(0)));
+
+        //yada
+        assertTrue(response.asString().contains("bookingid"));
+    }
+}
--- src/test/java/base_urls/HerokuAppBaseUrl.java
+++ src/test/java/base_urls/HerokuAppBaseUrl.java
@@ -5,13 +5,14 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
-public class JsonPlaceHolderBaseUrl {
+public class HerokuAppBaseUrl {
+
     //setUp methodu ile tekrarlı yapılacak işlemleri topluyoruz. Ve test öncesi çalışacak şekilde @Before anotasyonu ekliyoruz.
 
     protected RequestSpecification spec;
 
     @Before //Her test methodu öncesi çalışır
     public void setUp() throws Exception {
-        spec = new RequestSpecBuilder().setAccept(ContentType.JSON).setBaseUri("https://jsonplaceholder.typicode.com/").build();
+        spec = new RequestSpecBuilder().setAccept(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com/").build();
     }
 }
