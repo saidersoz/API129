ae5a866107365b355890e8c2f1b444233960bb73
saidErsoz <m.said.ersoz3443@gmail.com>
2023-05-25 23:45:47 +0300

    25/05

--- src/test/java/Get_Requests/Get06.java
+++ src/test/java/Get_Requests/Get06.java
@@ -2,11 +2,15 @@ package Get_Requests;
 
 import base_urls.HerokuAppBaseUrl;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
+import org.testng.asserts.SoftAssert;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Get06 extends HerokuAppBaseUrl {
 
@@ -53,19 +57,38 @@ public class Get06 extends HerokuAppBaseUrl {
                         "bookingdates.checkin", equalTo("2018-01-01"),
                         "bookingdates.checkout", equalTo("2019-01-01"),
                         "additionalneeds",equalTo("super bowls"));
+
+
+        //2. Yol: Json Path
+        JsonPath jsonPath = response.jsonPath();//jsonPath() methodu ile response'ı jsonPath objesine çevirdik.
+
+        //jsonPath objesi ile dataya spesifik olarak ulaşabiliriz:
+        assertEquals("Josh", jsonPath.getString("firstname"));
+        assertEquals("Allen", jsonPath.getString("lastname"));
+        assertEquals(111, jsonPath.getInt("totalprice"));
+        assertTrue(jsonPath.getBoolean("depositpaid"));
+        assertEquals("2018-01-01", jsonPath.getString("bookingdates.checkin"));
+        assertEquals("2019-01-01", jsonPath.getString("bookingdates.checkout"));
+        assertEquals("super bowls", jsonPath.getString("additionalneeds"));
+
+
+
+
+
+
+        //3. Yol: TestNG Soft Assertion
+        //Soft Assertion adımları:
+        //1. Soft assert objesi oluştur
+        SoftAssert softAssert = new SoftAssert();
+        //2. Assertion yap
+        softAssert.assertEquals(jsonPath.getString("firstname"),"Josh","firstname uyuşmadı");
+        softAssert.assertEquals(jsonPath.getString("lastname"),"Allen","lastname uyuşmadı");
+        softAssert.assertEquals(jsonPath.getInt("totalprice"),111,"totalprice uyuşmadı");
+        softAssert.assertTrue(jsonPath.getBoolean("depositpaid"));
+        //3. assertAll() methodunu kullan
+        softAssert.assertAll();
     }
 
-    @Test
-    public void test06() {
-        //Set the url
-        spec.pathParams("first", "booking", "second", 23);
 
-        //Set the expected data
-
-        //Send the request and get the response
-        Response response = given(spec).get("{first}/{second}");
-        response.prettyPrint();
-
-        //Do assertion
-    }
 }
+
