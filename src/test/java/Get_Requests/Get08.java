d3fe141fb08a6613f101dd2f659bd7a0e2b215f0
saidErsoz <m.said.ersoz3443@gmail.com>
2023-05-27 21:44:06 +0300

    27/05

--- src/test/java/Get_Requests/Get08.java
+++ src/test/java/Get_Requests/Get08.java
@@ -1,10 +1,15 @@
 package Get_Requests;
 
 import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
 import test_data.JsonPlaceHolderTestData;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Get08 extends JsonPlaceHolderBaseUrl {
 
@@ -37,6 +42,30 @@ public class Get08 extends JsonPlaceHolderBaseUrl {
         //Set the expected data
         JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
         Map<String, Object> expectedData = obj.expectedDataMapMethod(1, "quis ut nam facilis et officia qui", false);
+
+        expectedData.put("Via","1.1 vegur");
+        expectedData.put("Server","cloudflare");
+
         System.out.println("expectedData = " + expectedData);
+
+        //Send The Request and get the response
+        Response response = given(spec).get("{first}/{second}");
+        response.prettyPrint();
+
+        //Do assertion
+        Map<String, Object> actualData = response.as(HashMap.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+
+        //            And header "Via" is "1.1 vegur"
+        assertEquals(expectedData.get("Via"), response.header("Via")); //Assertion işlemini sabir değerler ile yapmalıyız
+
+        //            And header "Server" is "cloudflare"
+        assertEquals(expectedData.get("Server"), response.header("Server"));
+
     }
 }
